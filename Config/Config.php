0323e545cd09fb0f27de5f55bc6337ddf14d7fc1
leombpunk <leomb_punk@hotmail.com>
2023-01-08 01:29:01 -0300

    Update Config.php

--- Config/Config.php
+++ Config/Config.php
@@ -1,5 +1,5 @@
 <?php
-const BASE_URL = 'http://127.0.0.1/practicaMVC/';
+const BASE_URL = 'http://127.0.0.1/sistema-pos-php-mysql-valiadmin/';
 date_default_timezone_set('America/Argentina/Buenos_Aires');
 const DB_HOST = "127.0.0.1";
 const DB_NAME = "softboos";
