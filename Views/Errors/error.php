cbed71faaebcf480b1e2fffde705b3954f6cc36f
leombpunk <leomb_punk@hotmail.com>
2023-03-25 14:58:36 -0300

    preparando nuevos modulos

--- Views/Errors/error.php
+++ Views/Errors/error.php
@@ -2,8 +2,9 @@
 <main class="app-content">
 	<div class="page-error tile">
 	<h1><i class="fa fa-exclamation-circle"></i> Error 404: Page not found</h1>
+	<p>La página que usted solicitó no se encontró.</p>
 	<p>The page you have requested is not found.</p>
-	<p><a class="btn btn-primary" href="javascript:window.history.back();">Go Back</a></p>
+	<p><a class="btn btn-primary" href="javascript:window.history.back();">Volver</a></p>
 	</div>
 </main>
 <?php footerAdmin($data); ?>
--- Views/Template/nav_admin.php
+++ Views/Template/nav_admin.php
@@ -10,31 +10,30 @@
     <ul class="app-menu">
         <li><a class="app-menu__item" href="<?= base_url(); ?>dashboard"><i class="app-menu__icon fa fa-home"></i><span class="app-menu__label">Dashboard</span></a></li>
         <li><a class="app-menu__item" href="<?= base_url(); ?>ventas/nuevaVenta"><i class="app-menu__icon fa fa-shopping-cart" aria-hidden="true"></i><span class="app-menu__label">Nueva Venta</span></a></li>
+        <li><a class="app-menu__item" href="<?= base_url(); ?>compras/nuevaCompra"><i class="app-menu__icon fa fa-shopping-cart" aria-hidden="true"></i><span class="app-menu__label">Nueva Compra</span></a></li>
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-exchange" aria-hidden="true"></i><span class="app-menu__label">Movimientos</span><i class="treeview-indicator fa fa-angle-right"></i></a>
             <ul class="treeview-menu">
                 <li><a class="treeview-item" href="<?= base_url(); ?>movimientosCaja"><i class="icon fa fa-circle-o"></i> Caja</a></li>
                 <li><a class="treeview-item" href="<?= base_url(); ?>ventas"><i class="icon fa fa-circle-o"></i> Ventas</a></li>
-                <!-- <li><a class="treeview-item" href="<?= base_url(); ?>clientes"><i class="icon fa fa-circle-o"></i> Compras</a></li> -->
+                <li><a class="treeview-item" href="<?= base_url(); ?>compras"><i class="icon fa fa-circle-o"></i> Compras</a></li>
                 <li><a class="treeview-item" href="<?= base_url(); ?>formasPago"><i class="icon fa fa-circle-o"></i> Formas de Pago</a></li>
             </ul>
         </li>
-        <!-- <li><a class="app-menu__item" href="<?//= base_url(); ?>compras"><i class="app-menu__icon fa fa-shopping-cart" aria-hidden="true"></i><span class="app-menu__label">Compras</span></a></li> -->
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-users" aria-hidden="true"></i><span class="app-menu__label">Personas</span><i class="treeview-indicator fa fa-angle-right"></i></a>
             <ul class="treeview-menu">
                 <li><a class="treeview-item" href="<?= base_url(); ?>cargos"><i class="icon fa fa-circle-o"></i> Cargos</a></li>
                 <li><a class="treeview-item" href="<?= base_url(); ?>clientes"><i class="icon fa fa-circle-o"></i> Clientes</a></li>
                 <li><a class="treeview-item" href="<?= base_url(); ?>empleados"><i class="icon fa fa-circle-o"></i> Empleados</a></li>
-                <!-- <li><a class="treeview-item" href="<?//= base_url(); ?>proveedores"><i class="icon fa fa-circle-o"></i> Proveedores</a></li> -->
+                <li><a class="treeview-item" href="<?= base_url(); ?>proveedores"><i class="icon fa fa-circle-o"></i> Proveedores</a></li>
                 <!-- <li><a class="treeview-item" href="<?//= base_url(); ?>sucursales"><i class="icon fa fa-circle-o"></i> Sucursales</a></li> -->
             </ul>
         </li>
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-archive" aria-hidden="true"></i><span class="app-menu__label">Inventario</span><i class="treeview-indicator fa fa-angle-right"></i></a>
             <ul class="treeview-menu">
+                <li><a class="treeview-item" href="<?= base_url(); ?>productos"><i class="icon fa fa-circle-o"></i> Productos</a></li>
+                <li><a class="treeview-item" href="<?= base_url(); ?>combos"><i class="icon fa fa-circle-o"></i>Combos</a></li>
                 <li><a class="treeview-item" href="<?= base_url(); ?>rubros"><i class="icon fa fa-circle-o"></i> Rubros</a></li>
                 <li><a class="treeview-item" href="<?= base_url(); ?>udMedidas"><i class="icon fa fa-circle-o"></i> Unidades de Medidas</a></li>
-                <li><a class="treeview-item" href="<?= base_url(); ?>productos"><i class="icon fa fa-circle-o"></i> Productos</a></li>
-                <!-- <li><a class="treeview-item" href="<?//= base_url(); ?>productosVentas"><i class="icon fa fa-circle-o"></i> A la Venta</a></li> -->
-                <!-- <li><a class="treeview-item" href="<?//= base_url(); ?>cargos"><i class="icon fa fa-circle-o"></i> Recetas</a></li> -->
             </ul>
         </li>
         <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-bar-chart" aria-hidden="true"></i><span class="app-menu__label">Informes</span><i class="treeview-indicator fa fa-angle-right"></i></a>
@@ -47,11 +46,6 @@
                 <li><a class="treeview-item" href="<?//= base_url(); ?>clientes"><i class="icon fa fa-circle-o"></i> Clientes</a></li>
             </ul>
         </li> -->
-        <!-- <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-money" aria-hidden="true"></i><span class="app-menu__label">Tesorería</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-            <ul class="treeview-menu">
-                <li><a class="treeview-item" href="<?//= base_url(); ?>clientes"><i class="icon fa fa-circle-o"></i> Clientes</a></li>
-            </ul>
-        </li> -->
-        <!-- <li><a class="app-menu__item" href="<?//= base_url(); ?>dashboard"><i class="app-menu__icon fa fa-cog"></i><span class="app-menu__label">Opciones</span></a></li> -->
+        <li><a class="app-menu__item" href="<?= base_url(); ?>opciones"><i class="app-menu__icon fa fa-cog"></i><span class="app-menu__label">Opciones</span></a></li>
         <li><a class="app-menu__item" href="<?= base_url();?>logout"><i class="app-menu__icon fa fa-sign-out"></i><span class="app-menu__label"> Salir</span></a></li>
 </aside>
